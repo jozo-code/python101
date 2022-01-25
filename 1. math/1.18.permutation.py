5667851b0d9eeca6a05f07264075e114c427987c
jozo-code <koki2009@naver.com>
2022-01-25 16:23:11 +0900

    Math1. basic

---
+++ 1. math/1.18.permutation.py
@@ -0,0 +1,16 @@
+# 순열 구하기
+
+# nPr = n(n-1)(n-2)(n-r+1)
+
+numN=int(input("num 입력:"))
+numR=int(input("numR 입력:"))
+result=1
+
+for n in range(numN, (numN-numR),-1) :# 세번째 -1은 1씩 차감하는 조치임
+    # for n in range((numN-numR)+1,numN+1) 이렇게 쓴 거랑 똑같은데 왜그런지 모르겠네?
+    print("numN:{}".format(numN))
+    print("n:{}".format(n))
+    result = result*n
+
+print("result:{}".format(result))
+
