c1dd23b573d880603c3a9c4309fc741e07b488c5
jozo-code <koki2009@naver.com>
2022-01-25 17:10:01 +0900

    Math1. basic

--- 1. math/1.18.permutation.py
+++ 1. math/1.18.permutation.py
@@ -6,8 +6,11 @@ numN=int(input("num 입력:"))
 numR=int(input("numR 입력:"))
 result=1
 
+#range(2,7) # 2~6까지
+#range(7,2,-1) #7~3까지
+
 for n in range(numN, (numN-numR),-1) :# 세번째 -1은 1씩 차감하는 조치임
-    # for n in range((numN-numR)+1,numN+1) 이렇게 쓴 거랑 똑같은데 왜그런지 모르겠네?
+                            #for n in range((numN-numR)+1,numN+1): #이렇게 쓴 거랑 똑같은데 왜그런지 모르겠네?
     print("numN:{}".format(numN))
     print("n:{}".format(n))
     result = result*n
