aadbac39989ee5e4d0fda4660da7773cad4a5d01
jozo-code <koki2009@naver.com>
2022-01-25 10:26:14 +0900

    Math1. basic

--- 1. math/1.4.Factorization.py
+++ 1. math/1.4.Factorization.py
@@ -5,22 +5,16 @@ inputNumber = int(input("1보다 큰 정수 입력: "))
 
 n=2
 searchNumbers =[] #리스트 만들기
-while n <= inputNumber:
+while n <= inputNumber: #3 #3
     if inputNumber % n == 0: #소인수분해하는 과정
         print("소인수:{}".format(n))
         if searchNumbers.count(n) == 0: #searchNumbers에 n의 개수를 세서 0개 이면
-            searchNumbers.append(n) #searchNumbers 리스트에 n을 추가한다.
-
-            #여기서부터
-
+            searchNumbers.append(n) #searchNumbers 리스트에 n을 추가한다. #{3}
         elif searchNumbers.count(n) ==1: #n이 하나 들어있다면 
             searchNumbers.remove(n) #n을 지워버린다.
-
-            #여기까지는 지워도 똑같이 작동
-
-        inputNumber /= n #나눠서 계속 소인수분해하기
+        inputNumber /= n #나눠서 계속 소인수분해하기  #1
     else:
-        n = n+1 #또는 n+=1 이라고 써도 됨
+        n = n+1 #또는 n+=1 이라고 써도 됨 
 
 print("searchNumbers: {}".format(searchNumbers)) #searchNumbers에 담긴 숫자를 출력한다.
 
