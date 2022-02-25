ccf7f76459231233f6ca569d72ef00ad59b943f8
jozo-code <koki2009@naver.com>
2022-02-25 22:22:46 +0900

    test

--- 1. math/1.01.Factors and decimals1.py
+++
@@ -1,8 +0,0 @@
-# 파이썬을 이용해서 사용자가 입력한 숫자의 약수를 출력해보자
-
-inputNumber = int(input("0보다 큰 정수 입력: "))
-
-
-for number in range(1, inputNumber+1) :
-    if inputNumber % number == 0:
-        print("{}의 약수: {}".format(inputNumber,number))
